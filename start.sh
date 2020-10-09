07c0c8ceb27c02f3c83e750b888a5f9dc3147b52
madhukar tryambake <madhukar.tryambake@gmail.com>
2020-10-09 13:34:52 +0530

    removed names

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export DB_URL=postgres://***REMOVED***:***REMOVED***@172.17.0.2:5432/friezechat?sslmode=disable
+export DB_URL=postgres://keychatusr1:password@172.17.0.2:5432/friezechat?sslmode=disable
 export MATRIX_URL=localhost:8008
 export FRIEZE_NOTIFIER_LOG_FILE=t.txt
 
